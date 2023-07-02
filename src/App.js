afb41b38caeb577a0c09efedb959593853604045
JustKooba <jakubsakw2009@gmail.com>
2023-07-02 21:06:36 +0200

    Player can now win

--- src/App.js
+++ src/App.js
@@ -12,8 +12,15 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
+  let gameMessage;
+  if (winner) {
+    gameMessage = "Winner: " + winner;
+  } else {
+    gameMessage = "Next player: " + (xIsNext ? "X" : "O");
+  }
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -27,9 +34,11 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+
   return (
     <>
       <h1 className="title">React Tic-tac-toe app</h1>
+      <h2 className="winner">{gameMessage}</h2>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -47,4 +56,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
